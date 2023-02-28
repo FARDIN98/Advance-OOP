361d45ac144ac76418996349141fba95656e4af4
FARDIN98 <fardinahmed66@gmail.com>
2023-03-01 02:23:24 +0600

    code done

--- Javafx-practice/src/main/java/com/example/javafxpractice/HelloController.java
+++ Javafx-practice/src/main/java/com/example/javafxpractice/HelloController.java
@@ -1,8 +1,16 @@
 package com.example.javafxpractice;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -30,6 +38,18 @@ public class HelloController {
         double res = Double.parseDouble(s3) * Double.parseDouble(s4);
         showlabel.setText(res + "");
     }
+
+    @FXML
+    private void changeScene(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("sample2.fxml"));
+        Scene tableViewScene = new Scene(parent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
+    }
+
 
     @FXML
     private Label showlabel;
